0b1fc2a7d8fbfa3431545151ac1c06929db4fe13
Hieuhd26 <trunghieutranst@gmail.com>
2024-06-09 16:16:42 +0700

    navigation

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import { TrashScreen } from "./screens/TrashScreen";
 import { FolderScreen } from "./screens/FoldersScreen";
 import { LabelsScreen } from "./screens/LabelsScreen";
 import { EditNoteScreen } from "./screens/EditNoteScreen";
+import { AddNewNote } from "./screens/AddNewNote";
 
 import { TrashNoteProvider } from "./store/context/TrashContext";
 import { LabelProvider } from "./store/context/LabelContext";
@@ -20,13 +21,46 @@ import { LabelProvider } from "./store/context/LabelContext";
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
-function HomeStackNavigator() {
+function Root() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="EditNote" component={EditNoteScreen} />
-      {/*thêm vào đang bí  */}
-    </Stack.Navigator>
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Entypo name="home" size={24} color="black" />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Trash"
+        component={TrashScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Entypo name="trash" size={24} color="black" />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Folders"
+        component={FolderScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Entypo name="folder" size={24} color="black" />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Labels"
+        component={LabelsScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="label" size={24} color="black" />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
   );
 }
 
@@ -37,56 +71,22 @@ export default function App() {
       <LabelProvider>
         <TrashNoteProvider>
           <NavigationContainer>
-            <Drawer.Navigator
-              initialRouteName="Home"
-              screenOptions={{
-                drawerActiveBackgroundColor: "#e4baa1", 
-               
-              }}
-            >
-              <Drawer.Screen
-                name="Home"
-                component={HomeScreen}
-                options={{
-                  drawerIcon: ({ color, size }) => (
-                    <Entypo name="home" size={24} color="black" />
-                  ),
-                }}
+            <Stack.Navigator>
+              <Stack.Screen
+                name="Root"
+                component={Root}
+                options={{ headerShown: false }}
               />
-              <Drawer.Screen
-                name="Trash"
-                component={TrashScreen}
-                options={{
-                  drawerIcon: ({ color, size }) => (
-                    <Entypo name="trash" size={24} color="black" />
-                  ),
-                }}
-              />
-              <Drawer.Screen
-                name="Folders"
-                component={FolderScreen}
-                options={{
-                  drawerIcon: ({ color, size }) => (
-                    <Entypo name="folder" size={24} color="black" />
-                  ),
-                }}
-              />
-              <Drawer.Screen
-                name="Labels"
-                component={LabelsScreen}
-                options={{
-                  drawerIcon: ({ color, size }) => (
-                    <MaterialIcons name="label" size={24} color="black" />
-                  ),
-                }}
-              />
-            </Drawer.Navigator>
+              <Stack.Screen name="Edit Note" component={EditNoteScreen} />
+              <Stack.Screen name="Add Note" component={AddNewNote} />
+            </Stack.Navigator>
           </NavigationContainer>
         </TrashNoteProvider>
       </LabelProvider>
     </>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -96,3 +96,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+
+
